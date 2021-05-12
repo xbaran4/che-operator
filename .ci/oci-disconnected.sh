aa59a259cf45218940695ca706a7adb05673b35a
Flavius Lacatusu <flacatus@redhat.com>
2021-05-12 11:46:45 +0200

    kubectl install
    Signed-off-by: Flavius Lacatusu <flacatus@redhat.com>

--- .ci/oci-disconnected.sh
+++ .ci/oci-disconnected.sh
@@ -85,6 +85,109 @@ fi
 # Login to internal registry using podman
 podman login -u "${INTERNAL_REG_USERNAME}" -p "${INTERNAL_REG_PASS}" --tls-verify=false ${INTERNAL_REGISTRY_URL} --authfile=${REG_CREDS}
 
+# Build che-machine-exec,che-plugin-registry and che-devfile-registry from Github Sources
+# Che-Devfile-Registry Build
+git clone git@github.com:eclipse/che-devfile-registry.git
+cd che-devfile-registry
+
+./build.sh --organization "${ORGANIZATION}" \
+           --registry "${INTERNAL_REGISTRY_URL}" \
+           --tag "${TAG_NIGHTLY}" \
+           --offline
+cd .. && rm -rf che-devfile-registry
+
+# Che-Plugin-Registry-Build
+git clone git@github.com:eclipse-che/che-plugin-registry.git
+cd che-plugin-registry
+
+export SKIP_TEST=true && ./build.sh --organization "${ORGANIZATION}" \
+           --registry "${INTERNAL_REGISTRY_URL}" \
+           --tag "${TAG_NIGHTLY}" \
+           --offline \
+           --skip-digest-generation
+
+cd .. && rm -rf che-plugin-registry
+
+# Build Che-Machine-Exec
+git clone git@github.com:eclipse-che/che-machine-exec.git
+cd che-machine-exec
+podman build -f build/dockerfiles/Dockerfile -t "${INTERNAL_REGISTRY_URL}"/"${ORGANIZATION}"/che-machine-exec:"${TAG_NIGHTLY}" .
+cd .. && rm -rf che-machine-exec
+
+# Push che-machine-exec, devfile and plugins image to private registry
+podman push --authfile="${REG_CREDS}" --tls-verify=false "${INTERNAL_REGISTRY_URL}"/"${ORGANIZATION}"/che-devfile-registry:"${TAG_NIGHTLY}"
+podman push --authfile="${REG_CREDS}" --tls-verify=false "${INTERNAL_REGISTRY_URL}"/"${ORGANIZATION}"/che-plugin-registry:"${TAG_NIGHTLY}"
+podman push --authfile="${REG_CREDS}" --tls-verify=false "${INTERNAL_REGISTRY_URL}"/"${ORGANIZATION}"/che-machine-exec:"${TAG_NIGHTLY}"
+
+# Get all containers images used in eclipse-che deployment(postgresql, che-server-che-broket...)
+curl -sSLo- https://raw.githubusercontent.com/eclipse-che/che-operator/main/deploy/operator.yaml > /tmp/yam.yaml
+export ARRAY_OF_IMAGES=$(cat /tmp/yam.yaml | yq '.spec.template.spec.containers[0].env[] | select(.name|test("RELATED_")) | .value' -r)
+
+# Remove from Array of images devfile and plugins because will be builded using build.sh in offline mode.
+for delete in 'quay.io/eclipse/che-plugin-registry:nightly' 'quay.io/eclipse/che-devfile-registry:nightly'
+do
+    #Quotes when working with strings
+    ARRAY_OF_IMAGES=("${ARRAY_OF_IMAGES[@]/$delete}")
+done
+
+# Copy all che components to internal registry
+for IMAGE in ${ARRAY_OF_IMAGES[@]};
+do
+    echo -e "[INFO] Copying image ${IMAGE} to internal registry..."
+    if [[ "$IMAGE" =~ ^registry.access.redhat.com* ]]; then
+        IMG_VALUE=$(echo $IMAGE | sed -e "s/registry.access.redhat.com/""/g")
+        sudo skopeo copy --authfile=${REG_CREDS} --dest-tls-verify=false docker://"${IMAGE}" docker://"${INTERNAL_REGISTRY_URL}/eclipse${IMG_VALUE}"
+    fi
+
+    if [[ "$IMAGE" =~ ^quay.io* ]]; then
+        IMG_VALUE=$(echo $IMAGE | sed -e "s/quay.io/"${INTERNAL_REGISTRY_URL}"/g")
+        sudo skopeo copy --authfile=${REG_CREDS} --dest-tls-verify=false docker://"${IMAGE}" docker://"${IMG_VALUE}"
+    fi
+done
+
+# Copy Che Operator into private registry
+sudo skopeo copy --authfile=${REG_CREDS} --dest-tls-verify=false docker://quay.io/eclipse/che-operator:nightly docker://${INTERNAL_REGISTRY_URL}/eclipse/che-operator:nightly
+
+# OBTAIN PLUGINS
+IFS=$'\r\n' GLOBIGNORE='*' command eval 'PLUGINS_IMAGES=($(podman run --authfile=${XDG_RUNTIME_DIR}/containers/auth.json -it --rm \
+  --entrypoint cat ${INTERNAL_REGISTRY_URL}/eclipse/che-plugin-registry:nightly  /var/www/html/v3/external_images.txt))'
+for container in "${PLUGINS_IMAGES[@]}"
+do
+    :
+    if [[ $container != *"che-plugin-sidecar"* ]] &&
+       [[ $container != *"che-editor"* ]] && \
+       [[ $container != *"che-machine-exec"* ]] && \
+       [[ $container != *"codercom"* ]] && \
+       [[ $container != "docker.io"* ]]; then
+        REGISTRY_IMG_NAME=$(echo $container | sed -e "s/quay.io/"${INTERNAL_REGISTRY_URL}"/g")
+        sudo skopeo copy --authfile=${REG_CREDS} --dest-tls-verify=false docker://"${container}" docker://"${REGISTRY_IMG_NAME}"
+    fi
+done
+
+# Obtain SIDECAR_IMAGE
+podman run --authfile=${XDG_RUNTIME_DIR}/containers/auth.json -it --rm \
+  --entrypoint cat "${INTERNAL_REGISTRY_URL}"/eclipse/che-plugin-registry:nightly  /var/www/html/v3/plugins/golang/go/latest/meta.yaml > /tmp/workspace.yaml
+
+export SIDECAR_IMAGE=$(cat /tmp/workspace.yaml | yq '.spec.containers[] | .image' -r)
+
+if [[ "$SIDECAR_IMAGE" =~ ^quay.io* ]]; then
+    REGISTRY_IMG_NAME=$(echo $SIDECAR_IMAGE | sed -e "s/quay.io/"${INTERNAL_REGISTRY_URL}"/g")
+    echo $SIDECAR_IMAGE
+    sudo skopeo copy --authfile=${REG_CREDS} --dest-tls-verify=false docker://"${SIDECAR_IMAGE}" docker://"${REGISTRY_IMG_NAME}"
+fi
+
+# Obtain golang devfile
+IFS=$'\r\n' GLOBIGNORE='*' command eval 'DEVFILE_IMAGES=($(podman run --authfile=${XDG_RUNTIME_DIR}/containers/auth.json -it --rm \
+  --entrypoint cat ${INTERNAL_REGISTRY_URL}/eclipse/che-devfile-registry:nightly /var/www/html/devfiles/external_images.txt))'
+
+for container in "${DEVFILE_IMAGES[@]}"
+do
+    if [[ $container == *"che-golang"* ]]; then
+        REGISTRY_IMG_NAME=$(echo $container | sed -e "s/quay.io/"${INTERNAL_REGISTRY_URL}"/g")
+        sudo skopeo copy --authfile=${REG_CREDS} --dest-tls-verify=false docker://"${container}" docker://"${REGISTRY_IMG_NAME}"
+    fi
+done
+
 # Get the ocp domain for che custom resources
 export DOMAIN=$(oc get dns cluster -o json | jq .spec.baseDomain | sed -e 's/^"//' -e 's/"$//')
 
